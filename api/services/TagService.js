c4e042c1d98340592a63001f22b4e2ac16cd642b
Ernesto Rojas <ernesto20145@gmail.com>
2019-01-25 15:01:54 -0300

    Bug fixed.

--- api/services/TagService.js
+++ api/services/TagService.js
@@ -66,7 +66,9 @@ async function processVideoMediaConvert(content) {
   const { lowQualityURL, mediaQualityURL } = result;
   const video = ObjectModel.findOne({ uuid });
   if (video) {
-    cano.log.debug(JSON.stringify(video));
+    cano.log.debug(video.uuid);
+    cano.log.debug(video.name);
+    cano.log.debug(video.id);
     const res = await ObjectModel.updateOne({ _id: video.id }, { $set: { lowQualityURL, mediaQualityURL } });
     cano.log.debug('ObjectModel.updateOne -> result', JSON.stringify(res));
   } else {
